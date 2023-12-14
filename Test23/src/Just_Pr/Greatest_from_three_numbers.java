7a0f1362bae9274ff5ab219991fc35d6f5146aa5
Snehal Gulhane <Snehal231194>
2023-12-14 11:04:22 +0530

    ee

---
+++ Test23/src/Just_Pr/Greatest_from_three_numbers.java
@@ -0,0 +1,38 @@
+package Just_Pr;
+
+public class Greatest_from_three_numbers {
+
+	
+	
+	public static void main(String[] args) {
+		int a=10,b=20,c=30;
+		if (a>b) { //10>20
+			if (b>c) {
+				System.out.println("Greatest no is" + b);
+			} else {
+
+				System.out.println("Greatest no is" + c);
+				
+			}
+			
+		}
+		
+		else {
+			if (b>c) {
+				System.out.println("Greatest no is" + b);
+			} else {
+				System.out.println("Greatest no is" + c);
+			}
+
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
